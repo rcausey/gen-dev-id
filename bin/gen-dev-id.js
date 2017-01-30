35d52dd4e094035c65d155d300c16a6273a77f7f
rcausey <Rhys.Causey@D2L.com>
2017-01-30 16:54:43 -0500

    Testing yards

--- bin/gen-dev-id.js
+++ bin/gen-dev-id.js
@@ -1,3 +1,13 @@
 #!/usr/bin/env node
 
-console.log('fjkal');
+const yargs = require('yargs');
+const argv = yargs
+    .usage('Usage: $0 -o [output path]')
+    .demandOption(['o'])
+    .argv;
+
+const fs = require('fs');
+const randomstring = require('randomstring');
+
+const root = fs.realpathSync(`${__dirname}/..`);
+console.log(root);
