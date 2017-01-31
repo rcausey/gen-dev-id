c4884e46889b5cb9666346519b5126717523ddc2
rcausey <Rhys.Causey@D2L.com>
2017-01-31 16:33:49 -0500

    Works now

--- bin/gen-dev-id.js
+++ bin/gen-dev-id.js
@@ -2,12 +2,26 @@
 
 const yargs = require('yargs');
 const argv = yargs
-    .usage('Usage: $0 -o [output path]')
+    .usage('Usage: $0 -o [output path] -len [num]')
     .demandOption(['o'])
+    .default('len', 5)
     .argv;
 
+const process = require('process');
 const fs = require('fs');
 const randomstring = require('randomstring');
 
-const root = fs.realpathSync(`${__dirname}/..`);
-console.log(root);
+const root = process.cwd();
+const outputPath = `${root}/${argv.o}`;
+
+const id = fs.existsSync(outputPath)
+  ? fs.readFileSync(outputPath)
+    .toString().replace(/\s*/g, '') // Remove whitespace added by editors
+  : randomstring.generate({
+    length: argv.len,
+    charset: '0123456789abcdefghijklmnopqrstuvwxyz',
+  });
+
+fs.writeFileSync(outputPath, id);
+
+console.log(`DEV_UNIQUE_ID = ${id}`); // eslint-disable-line no-console
